a2b289872b7d88f3ff87789524188ba3463d85c9
Jeremy Callinan <jeremy.callinan@gmail.com>
2024-10-01 19:12:46 -0400

    After page 34
    CloudCover started

--- Mission Demolition/Assets/FollowCam.cs
+++ Mission Demolition/Assets/FollowCam.cs
@@ -8,6 +8,7 @@ public class FollowCam : MonoBehaviour
 
     [Header("Inscribed")]
     public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
 
     [Header("Dynamic")]
     public float camZ;
@@ -19,12 +20,29 @@ public class FollowCam : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (POI== null) { return; }
-        Vector3 destination = POI.transform.position;
+       
+        Vector3 destination = Vector3.zero;
+        if (POI != null)
+        {
+            // if the POI has a rigidbody, check to see if it's sleeping
+            Rigidbody poiRigid = POI.GetComponent<Rigidbody>();
+            if ((poiRigid != null) && poiRigid.IsSleeping())
+            {
+                POI = null;
+            }
+        }
+
+        if (POI != null)
+        {
+            destination = POI.transform.position;
+        }
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
         destination = Vector3.Lerp(transform.position, destination, easing);
         destination.z = camZ;
         //set the camera's position
-        transform.position = destination;   
+        transform.position = destination;
+        Camera.main.orthographicSize = destination.y + 10;
     }
     // Start is called before the first frame update
     void Start()
