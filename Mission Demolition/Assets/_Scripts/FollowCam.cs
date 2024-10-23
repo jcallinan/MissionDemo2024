d21259da967f1a0943c3389f0bbb649be5e84c5b
Jeremy Callinan <jeremy.callinan@gmail.com>
2024-10-22 20:21:59 -0400

    After completion

--- Mission Demolition/Assets/_Scripts/FollowCam.cs
+++ Mission Demolition/Assets/_Scripts/FollowCam.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class FollowCam : MonoBehaviour
 {
+    static private FollowCam S;
     static public GameObject POI; // point of interest
+
+    public enum eView { none, slingshot, castle, both};
 
     [Header("Inscribed")]
     public float easing = 0.05f;
     public Vector2 minXY = Vector2.zero;
+    public GameObject viewBothGO;
 
     [Header("Dynamic")]
     public float camZ;
-
+    public eView nextView = eView.slingshot;
     void Awake()
     {
+        S = this;
         camZ = this.transform.position.z;   
     }
 
@@ -54,5 +59,35 @@ public class FollowCam : MonoBehaviour
     void Update()
     {
         
+    }
+    public void SwitchView(eView newView)
+    {
+        if (newView == eView.none)
+        {
+            newView = nextView;
+        }
+        switch (newView)
+        {
+            case eView.slingshot:
+                POI = null;
+                nextView = eView.castle;
+                break;
+            case eView.castle:
+                POI = MissionDemolition.GET_CASTLE();
+                nextView = eView.both;
+                break;
+            case eView.both:
+                POI = viewBothGO;
+                nextView = eView.slingshot;
+                break;  
+        }
+    }
+    public void SwitchView()
+    {
+        SwitchView(eView.none); 
+    }
+    static public void SWITCH_VIEW(eView newView)
+    {
+        S.SwitchView(newView);
     }
 }
--- Mission Demolition/Assets/_Scripts/Projectile.cs
+++ Mission Demolition/Assets/_Scripts/Projectile.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     const int LOOKBACK_COUNT = 10;
+    static List<Projectile> PROJECTILES = new List<Projectile>();   
     [SerializeField]
     private bool _awake = true;
     public bool awake
@@ -25,7 +26,7 @@ public class Projectile : MonoBehaviour
         awake = true;
         prevPos = new Vector3(1000, 1000, 0);
         deltas.Add(1000);
-
+        PROJECTILES.Add(this);
     }
 
     // Update is called once per frame
@@ -56,5 +57,16 @@ public class Projectile : MonoBehaviour
 
 
 
+    }
+    private void OnDestroy()
+    {
+        PROJECTILES.Remove(this);
+    }
+    static public void DESTROY_PROJECTILES()
+    {
+        foreach (Projectile p in PROJECTILES)
+        {
+            Destroy(p.gameObject);
+        }
     }
 }
